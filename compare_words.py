ac151f3638259a506b314be6a770474719ec8129
Henry Baughman <hbaughman741@gmail.com>
2014-04-25 14:38:53 -0400

    creat within_bound system

--- compare_words.py
+++ compare_words.py
@@ -4,17 +4,23 @@ import pickle
 with open("word_path_defs.pickle", "rb") as handle:
     paths = pickle.load(handle)
 
+# compare vectors for similarity
 def path_match(word1, word2):
     for bigram_num, bigram in enumerate(word1):
         match_bigram = word2[bigram_num]
 
-        direction = bigram['direction'] != match_bigram['direction']
+        direction_diff = bigram['direction'] - match_bigram['direction']
+        dir_bounds = 10
+        within_direction_bounds = (-dir_bounds < direction_diff < dir_bounds or
+                                    direction_diff > 360 - dir_bounds or 
+                                    direction_diff < -360 + dir_bounds)
+
         magnitude_diff = bigram['magnitude'] - match_bigram['magnitude']
-        if direction or magnitude_diff < -2 or magnitude_diff > 2:
+        mag_bounds = 1.5
+        within_magnitude_bounds = -mag_bounds < magnitude_diff < mag_bounds
+
+        if not within_direction_bounds or not within_magnitude_bounds:
             return False
-            # print "bigram %s matches direction" % bigram_num
-            # print "word bigram:", bigram
-            # print "match bigram:", match_bigram
     else:
         return True
 
@@ -27,7 +33,9 @@ for layout_name, layout in paths.iteritems():
         for match in match_layout:
             if len(layout[word]) == len(match_layout[match]) and word != match:
                 if path_match(layout[word], match_layout[match]):
-                    print word, match
+                    print "  ", word, match
+                    # print "    word[0]:", layout[word][0]
+                    # print "    match[0]:", layout[match][0]
 
         # once something has been compared with everything remove it
         match_layout.pop(word, None)
